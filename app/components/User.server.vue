c8f785d4f507ea70137cc9cc159054d83e635251
Parth Kaushal <parthkaushal1008@gmail.com>
2025-10-09 08:27:09 +0530

    removed wrong file

--- app/components/User.server.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-   import { withAccelerate } from "@prisma/extension-accelerate";
-   const prisma = usePrismaClient().$extends(withAccelerate());
-   const user = await prisma.user.findFirst();
-</script>
-
-<template>
-  <p>{{ user?.name ?? "No user has been added yet." }}</p>
-</template>
